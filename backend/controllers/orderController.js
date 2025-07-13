d50f5189c9bd8b4d132a416a386a3a4a63d78378
Buddhika-Prasadgit config --global user.email buddhikaprasad3040@gmail.comgit config --listgit config --global user.name Buddhika-Prasad <buddhikaprasad3040@gmail.com>
2025-07-13 23:15:57 +0530

    implement create & get orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,21 +5,54 @@ import Order from "../models/orderModel.js";
 // @route   GET /api/orders
 // @access  Purivate
 const addOrderItems = asyncHandler(async (req, res) => {
-  res.send("add Order Items");
+ const { orderItems, shippingAddress, paymentMethod, itemsPrice, taxPrice, shippingPrice, totalPrice } = req.body;
+
+ if (orderItems && orderItems.length === 0) {
+  res.status(400);
+  throw new Error('No order items');
+  return;
+ }
+ const order = new Order({
+  orderItems: orderItems.map((x) => ({
+    ...x,
+    product: x._id,
+    _id: undefined
+      
+  }))
+  user: req.user._id,
+  shippingAddress,
+  paymentMethod,
+  itemsPrice,
+  taxPrice,
+  shippingPrice,
+  totalPrice
+ })
+ const createdOrder = await order.save();
+ res.status(201).json(createdOrder);
 });
 
 // @desc    get logged in user orders
 // @route   GET /api/orders/myorders
 // @access  Purivate
 const getMyOrders = asyncHandler(async (req, res) => {
-  res.send("get My Orders");
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json(orders);
 });
 
 // @desc    get order by id
 // @route   GET /api/orders/:id
 // @access  Purivate
 const getOrderById = asyncHandler(async (req, res) => {
-  res.send("get Order By Id");
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
 });
 
 // @desc    update order to paid
