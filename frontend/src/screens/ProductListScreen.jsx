a3cb25a497546d9c343bc65cd01ac210c604b41c
Buddhika-Prasadgit config --global user.email buddhikaprasad3040@gmail.comgit config --listgit config --global user.name Buddhika-Prasad <buddhikaprasad3040@gmail.com>
2025-07-18 19:22:32 +0530

    Add Paginate

--- frontend/src/screens/ProductListScreen.jsx
+++ frontend/src/screens/ProductListScreen.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
 import { Table, Button, Row, Col } from "react-bootstrap";
 import { FaEdit, FaTrash } from "react-icons/fa";
+import { useParams } from "react-router-dom";
 import Loader from "../components/Loader";
+import Paginate from "../components/Paginate";
 import { toast } from "react-toastify";
 import Message from "../components/Message";
 import {
@@ -11,7 +13,10 @@ import {
 } from "../slices/productsApiSlice";
 
 const ProductListScreen = () => {
-  const { data: products, isLoading, error, refetch } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+  const { data, isLoading, error, refetch } = useGetProductsQuery({
+    pageNumber,
+  });
 
   const [createProduct, { isLoading: loadingCreate }] =
     useCreateProductMutation();
@@ -79,7 +84,7 @@ const ProductListScreen = () => {
             </tr>
           </thead>
           <tbody>
-            {products.map((product) => (
+            {data.products.map((product) => (
               <tr key={product._id}>
                 <td>{product._id}</td>
                 <td>{product.name}</td>
@@ -104,6 +109,9 @@ const ProductListScreen = () => {
             ))}
           </tbody>
         </Table>
+      )}
+      {data && data.pages > 1 && (
+        <Paginate pages={data.pages} page={data.page} isAdmin={true} />
       )}
     </>
   );
